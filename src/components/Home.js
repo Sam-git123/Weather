9660be92390a6ba8beacf7cf87268410367c9865
Sameer Qureshi <sameer.qureshi.mitm@gmail.com>
2020-10-03 18:46:34 +0530

    error handlng and fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,98 @@
 import React, { useState } from "react";
 import CloudIcon from "@material-ui/icons/Cloud";
 import WeatherReport from "./WeatherReport";
+import { CircularProgress } from "@material-ui/core";
 
 const Home = () => {
-	const apiKey = '2f9da10777775685f6c591342a2a6324';
-	const [city, setCity] = useState("");
-	const [weather, setWeather] = useState({});
+  const apiKey = "2f9da10777775685f6c591342a2a6324";
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({
+		city: '',
+		country: '',
+		temp: '',
+		sunrise: '',
+		sunset: '',
+		min: '',
+		max: '',
+	});
+  const [error, setError] = useState("");
+  const [isLoading, setLoad] = useState(false);
+
   let changeCity = (event) => {
     setCity(event.target.value);
   };
 
-  const handleClick = () => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-				console.warn(data);
-        setWeather(data);
-      });
-  };
-  return (
+  const handleClick = async (e) => {
+		e.preventDefault();
+		try {
+			if (!city) {
+				alert("Please select a city");
+				setWeather({
+					city: '',
+					country: '',
+					temp: '',
+					sunrise: '',
+					sunset: '',
+					min: '',
+					max: '',
+				});
+				setError('');
+				return;
+			}
+			setLoad(true);
+			const API_call = await fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+			)
+			const response = await API_call.json();
+			if(response.cod === '404'){
+				setLoad(false);
+				setError(response.message); 
+			} 
+			else {
+				const result = {
+					city: response.name,
+					country: response.sys.country,
+					temp: response.main.temp,
+					sunrise: response.sys.sunrise,
+					sunset: response.sys.sunset,
+					min: response.main.temp_min,
+					max: response.main.temp_max,
+				}
+			setWeather(result);
+			setLoad(false);
+			setError('');
+			}
+		}
+		catch(err) {	
+			
+		}
+	};
+
+	const calcTemp = (temperature) => {
+		let temp = Math.floor(temperature - 273.10)
+		return temp;
+	}
+	
+  return(
     <div>
       <h3>Weather App</h3>
       <CloudIcon className="icon" />
-      <div>
+      <form onSubmit={handleClick}>
         <label>Please Select City:</label>
         <input type="text" value={city} onChange={changeCity} />
-        <button onClick={handleClick}>Search</button>
-      </div>
-      <WeatherReport weather={weather}/>
+        <button type="submit">Search</button>
+      </form>
+			{ isLoading ? <CircularProgress /> :
+			 error ? <h1>{error}</h1>:
+			 <WeatherReport 
+				city={weather.city}
+				country={weather.country}
+				temp={calcTemp(weather.temp)}
+				sunrise={weather.sunrise}
+				sunset={weather.sunset}
+				min={calcTemp(weather.min)}
+				max={calcTemp(weather.max)}
+			 />}
     </div>
   );
 };
--- src/components/WeatherReport.jsx
+++ src/components/WeatherReport.jsx
@@ -1,16 +1,18 @@
-import { Label } from '@material-ui/icons';
-import React from 'react';
+import React from "react";
+import moment from "moment";
 
 const WeatherReport = (props) => {
-    const { weather } = props;
-    return (
-     <div>
-       <Label>Weather for {weather.name}</Label>
-       {/* {weather ? Object.values(weather).map((e) => {
-         return e;
-       }) : null} */}
-     </div>   
-    )
-}
+  const { city, country, temp, sunset, sunrise, min, max } = props;
+
+  return (
+    <div>
+      {city && country && <h4>{city}, {country}</h4>}
+      {temp >= 0 && <h4>Temperature : {temp}&deg;</h4>}
+      {sunrise && <h4>Sunrise at: {moment.unix(sunrise).format("h:mm:ss a")}</h4>}
+      {sunset && <h4>Sunset at: {moment.unix(sunset).format("h:mm:ss a")}</h4>}
+      {min >= 0 && max >= 0 && <h4>Min: {min}&deg;   Max: {max}&deg;</h4>}
+    </div>
+  );
+};
 
 export default WeatherReport;
